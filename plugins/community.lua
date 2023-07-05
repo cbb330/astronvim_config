0b590756301c2cce52b3aa092d6914cb91b74232
Christian Bush <chbush@linkedin.com>
2023-07-04 22:26:00 -0700

    first stab at config

---
+++ plugins/community.lua
@@ -0,0 +1,14 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+}
