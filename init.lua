b16b2d04275d6828897f005466aebfc1cc447849
Christian Bush <chbush@linkedin.com>
2023-07-11 09:05:46 -0700

    good style, basic mappings, working on python

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
 return {
-  colorscheme = "tokyonight-storm",
+  -- colorscheme = "tokyonight-storm",
+  -- colorscheme = "everforest",
+  colorscheme = "onedark",
+  lsp = {
+    formatting = {
+      format_on_save = false, -- enable or disable automatic formatting on save
+    },
+  },
+  -- This function is run last and is a good place to configuring
+  -- augroups/autocommands and custom filetypes also this just pure lua so
+  -- anything that doesn't fit in the normal config locations above can go here
+  polish = function()
+    local augroup = vim.api.nvim_create_augroup
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      desc = "Auto-Select Virtualenv on Start",
+      pattern = "*",
+      group = augroup("venv-selector", { clear = true }),
+      callback = function() require("venv-selector").retrieve_from_cache() end,
+      once = true,
+    })
+  end,
 }
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,11 +1,31 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.python" },
+  { -- further customize the options set by the community
+    "venv-selector.nvim",
+    config = function()
+      require("venv-selector").setup {
+        name = {
+          key1 = "venv",
+          key2 = "development-venv",
+        },
+      }
+
+    end,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.iceberg-vim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.mellow-nvim" },
+  { import = "astrocommunity.colorscheme.onigiri-nvim" },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  { import = "astrocommunity.colorscheme.sonokai" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
